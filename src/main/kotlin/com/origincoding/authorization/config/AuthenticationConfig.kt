4418e8646da85043057873e6be9c94518467c0e7
origin-coding <1604070358@qq.com>
2025-01-07 20:13:52 +0800

    feat: Config password encoder.

--- src/main/kotlin/com/origincoding/authorization/config/AuthenticationConfig.kt
+++ src/main/kotlin/com/origincoding/authorization/config/AuthenticationConfig.kt
@@ -4,12 +4,16 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 
 @Configuration
 class AuthenticationConfig {
     @Bean
     fun userDetailsService(): UserDetailsService {
-        return InMemoryUserDetailsManager(User("user", "{noop}password", emptyList()))
+        return InMemoryUserDetailsManager(User("user", passwordEncoder().encode("password"), emptyList()))
     }
+
+    @Bean fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 }
