6f28b9af6bb581448ea5a2d4a4075ece8759b48d
origin-coding <1604070358@qq.com>
2025-01-08 21:58:52 +0800

    feat: Implement the easiest OAuth 2.0 authorization.

--- src/main/kotlin/com/origincoding/authorization/config/AuthenticationConfig.kt
+++ src/main/kotlin/com/origincoding/authorization/config/AuthenticationConfig.kt
@@ -1,19 +1,125 @@
 package com.origincoding.authorization.config
 
+import com.nimbusds.jose.jwk.JWKSet
+import com.nimbusds.jose.jwk.RSAKey
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet
+import com.nimbusds.jose.jwk.source.JWKSource
+import com.nimbusds.jose.proc.SecurityContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
+import org.springframework.http.MediaType
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.oauth2.core.AuthorizationGrantType
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod
+import org.springframework.security.oauth2.core.oidc.OidcScopes
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher
+import java.security.KeyPair
+import java.security.KeyPairGenerator
+import java.security.interfaces.RSAPrivateKey
+import java.security.interfaces.RSAPublicKey
+import java.util.*
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
+
+@ExperimentalUuidApi
 @Configuration
 class AuthenticationConfig {
+    @Bean
+    @Order(1)
+    fun authorizationServerSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        val authorizationServerConfigurer = OAuth2AuthorizationServerConfigurer.authorizationServer()
+
+        http.securityMatcher(authorizationServerConfigurer.endpointsMatcher)
+            .with(authorizationServerConfigurer) {
+                it.oidc(Customizer.withDefaults())
+            }
+            .authorizeHttpRequests { it.anyRequest().authenticated() }
+            .exceptionHandling {
+                it.defaultAuthenticationEntryPointFor(
+                    LoginUrlAuthenticationEntryPoint("/login"),
+                    MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+                )
+            }
+
+        return http.build()
+    }
+
+    @Bean
+    @Order(2)
+    @Throws(Exception::class)
+    fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain = http
+        // 全部的请求都需要认证
+        .authorizeHttpRequests { it.anyRequest().authenticated() }
+        // 使用默认表单登录
+        .formLogin(Customizer.withDefaults()).build()
+
+    @Bean
+    fun registeredClientRepository(): RegisteredClientRepository {
+        val oidcClient = RegisteredClient.withId(UUID.randomUUID().toString()).clientId("oidc-client")
+            .clientSecret(passwordEncoder().encode("secret"))
+            .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+            .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+            .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+            .redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
+            .postLogoutRedirectUri("http://127.0.0.1:8080/").scope(OidcScopes.OPENID).scope(OidcScopes.PROFILE)
+            .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+            .build()
+
+        return InMemoryRegisteredClientRepository(oidcClient)
+    }
+
+
     @Bean
     fun userDetailsService(): UserDetailsService {
         return InMemoryUserDetailsManager(User("user", passwordEncoder().encode("password"), emptyList()))
     }
 
-    @Bean fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+    @Bean
+    fun jwtSource(): JWKSource<SecurityContext> {
+        val keyPair = generateRsaKey()
+        val publicKey = keyPair.public as RSAPublicKey
+        val privateKey = keyPair.private as RSAPrivateKey
+        val rsaKey = RSAKey.Builder(publicKey)
+            .privateKey(privateKey)
+            .keyID(Uuid.random().toString())
+            .build()
+        return ImmutableJWKSet(JWKSet(rsaKey))
+    }
+
+    private fun generateRsaKey(): KeyPair {
+        val keyPairGenerator = KeyPairGenerator.getInstance("RSA")
+        keyPairGenerator.initialize(2048)
+        return keyPairGenerator.generateKeyPair()
+    }
+
+    @Bean
+    fun jwtDecoder(jwkSource: JWKSource<SecurityContext>): JwtDecoder {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource)
+    }
+
+    @Bean
+    fun authorizationServerSettings(): AuthorizationServerSettings {
+        return AuthorizationServerSettings.builder().build()
+    }
 }
